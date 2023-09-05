74bf254b5192851c77f51ae58edd7a219f688a4e
lopesalice <138293910+lopesalice@users.noreply.github.com>
2023-09-04 21:13:38 -0300

    primeira requisição

---
+++ primeiroServerDSW2023.2/src/main/java/servelets/PrimeiroServer.java
@@ -0,0 +1,18 @@
+package servelets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/primServlet")
+public class PrimeiroServer extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String nome = req.getParameter("nomeP");
+		System.out.println("Nome: " + nome);
+	}
+}
